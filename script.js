3a795b7a94a88277efe0362a6e52db6d8e31f900
Eduardo Verdusco <em.verdusco@gmail.com>
2021-03-04 11:09:29 -0800

    add script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+     function openNav() {
+      document.getElementById("navbar").style.cssText = "width: 17vw; display: block;";
+
+    }
+
+    function closeNav() {
+      document.getElementById("navbar").style.cssText = "width: 0; display: none;";
+    }
